38652c5117460808680aeca9a808a2d72e158cb1
Panpan Han <hanpanpan200@gmail.com>
2017-06-14 00:50:58 +0800

    feat: "sortWordByCount"

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,23 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by pphan on 13/06/2017.
  */
 public class Counter {
-    private final String[] words;
-
-    public Counter(String[] words) {
-        this.words = words;
-    }
-
-    public Map<String, Integer> count() {
+    public Map<String, Integer> count(String[] words) {
         Map<String, Integer> wordCounts = new HashMap<>();
 
-        for (String word: this.words) {
+        for (String word: words) {
             int count = wordCounts.containsKey(word) ? wordCounts.get(word) + 1 : 1;
             wordCounts.put(word, count);
         }
         return wordCounts;
+    }
+
+    public List<Map.Entry<String, Integer>> sort_word_by_count(Map<String, Integer> wordCounts) {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(wordCounts.entrySet());
+        Collections.sort(entries, (o1, o2) -> (o2.getValue() - o1.getValue()));
+        return entries;
     }
 }
--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -15,12 +14,45 @@ public class CounterTest {
     public void should_get_correct_word_count() {
         //Given
         String[] words = {"the", "day", "is", "the", "the"};
-        Counter counter = new Counter(words);
+        Counter counter = new Counter();
         //When
-        Map<String, Integer> result = counter.count();
+        Map<String, Integer> result = counter.count(words);
         //Then
         assertThat(result.get("the"),is(3));
         assertThat(result.get("day"),is(1));
         assertThat(result.get("is"),is(1));
+    }
+
+    @Test
+    public void should_sort_word_counts_in_descending_order() {
+        //Given
+        Map<String, Integer> wordCounts = new HashMap<>();
+        wordCounts.put("day", 1);
+        wordCounts.put("the", 3);
+        wordCounts.put("is", 2);
+        Counter counter = new Counter();
+        //When
+        List<Map.Entry<String, Integer>> result = counter.sort_word_by_count(wordCounts);
+        //Then
+        Iterator iterator = result.iterator();
+        Integer index = 0;
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            Object key = entry.getKey( );
+            Object value = entry.getValue();
+            if (index == 0) {
+                assertThat(key, is("the"));
+                assertThat(value, is(3));
+            }
+            if (index == 1) {
+                assertThat(key, is("is"));
+                assertThat(value, is(2));
+            }
+            if (index == 2) {
+                assertThat(key, is("day"));
+                assertThat(value, is(1));
+            }
+            index++;
+        }
     }
 }
